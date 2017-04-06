538822b2a1c979f8066b7bd1d3099e41b44cb828
alexandervansomeren <alexander.vansomeren@gmail.com>
2017-04-06 12:38:30 +0200

    Changed feature building for implicit negation (TODO: change logicnn_sentiment.py + fol.py)

--- data/logicnn_features.py
+++ data/logicnn_features.py
@@ -15,7 +15,7 @@ import time
 warnings.filterwarnings("ignore")
 
 
-def text_after_first(text, part):
+def text_first(text, part):
     if part in text:
         return ''.join(text.split(part)[1:])
     else:
@@ -26,14 +26,14 @@ def extract_features(revs):
     but_fea = []
     but_ind = []
     but_fea_cnt = 0
-    nt_fea_before = []
-    nt_fea_after = []
-    nt_ind = []
-    nt_fea_cnt = 0
+    implicit_negation_fea_before = []
+    implicit_negation_fea = []
+    implicit_negation_ind = []
+    implicit_negation_fea_cnt = 0
     for rev in revs:
         text = rev["text"]
-        nt_ind.append(0)
-        fea_after = ''
+        implicit_negation_ind.append(0)
+        fea = ''
         fea_before = ''
         if ' but ' in text:
             but_ind.append(1)
@@ -42,32 +42,48 @@ def extract_features(revs):
             fea = ''.join(fea)
             fea = fea.strip().replace('  ', ' ')
             but_fea_cnt += 1
+        elif ' however ' in text:
+            but_ind.append(1)
+            # make the text after 'but' as the feature
+            fea = text.split('but')[1:]
+            fea = ''.join(fea)
+            fea = fea.strip().replace('  ', ' ')
+            but_fea_cnt += 1
+        elif ' unfortunately ' in text:
+            but_ind.append(1)
+            # make the text after 'but' as the feature
+            fea = text.split('but')[1:]
+            fea = ''.join(fea)
+            fea = fea.strip().replace('  ', ' ')
+            but_fea_cnt += 1
         else:
             but_ind.append(0)
-            fea = ''
             # only if not but already
-            if " n't " in text:
-                nt_ind.append(1)
+            if " avoid" in text:
+                implicit_negation_ind.append(1)
                 # make the text after 'but' as the feature
-                fea_after = text.split("n't")[1:]
-                fea_before = text.split("n't")[:1]
-                fea_after = ''.join(fea_after)
-                fea_before = ''.join(fea_before)
-                fea_after = fea_after.strip().replace('  ', ' ')
-                fea_before = fea_before.strip().replace('  ', ' ')
-                nt_fea_cnt += 1
+                fea = text.split(' avoid')[1].split('.')[0].split(',')[0]
+                fea = fea.strip().replace('  ', ' ')
+                implicit_negation_fea_cnt += 1
+            elif " hardly" in text:
+                implicit_negation_ind.append(1)
+                # make the text after 'but' as the feature
+                fea = text.split(' hardly')[1].split('.')[0].split(',')[0]
+                fea = ''.join(fea)
+                fea = fea.strip().replace('  ', ' ')
+                implicit_negation_fea_cnt += 1
         but_fea.append(fea)
-        nt_fea_after.append(fea_after)
-        nt_fea_before.append(fea_before)
+        implicit_negation_fea.append(fea)
+        implicit_negation_fea_before.append(fea_before)
 
     print '#but %d' % but_fea_cnt
-    print '#n\'t %d' % nt_fea_cnt
+    print '#implicit negation: %d' % implicit_negation_fea_cnt
     return {
         'but_text': but_fea,
         'but_ind': but_ind,
-        'nt_before_text': nt_fea_before,
-        'nt_after_text': nt_fea_after,
-        'nt_ind': nt_ind,
+        'implicit_negation_before_text': implicit_negation_fea_before,
+        'implicit_negation_text': implicit_negation_fea,
+        'implicit_negation_ind': implicit_negation_ind,
     }
 
 
